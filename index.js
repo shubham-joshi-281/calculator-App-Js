24c2aa777005b273f9b60738ca051d649b770c12
Shubham <shubhamjoshi281@gmail.com>
2023-09-19 17:05:36 +0530

    java script file

---
+++ index.js
@@ -0,0 +1,11 @@
+function display(val) {
+  document.getElementById("display").value += val;
+}
+function solve() {
+  let x = document.getElementById("display").value;
+  let y = eval(x);
+  document.getElementById("display").value = y;
+}
+function clr() {
+  document.getElementById("display").value = " ";
+}
